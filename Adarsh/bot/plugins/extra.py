97e7225f2152cc69f7a678144cb57e4d6c8c99b5
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2023-11-02 11:30:06 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Coded By [Neha](https://t.me/anjel_neha)",
+                    text="I am Coded By [Sudip](https://t.me/SUDIPSAHA06)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Developer💻", url=f"https://t.me/anjel_neha")
+                                InlineKeyboardButton("Developer💻", url=f"https://t.me/SUDIPSAHA06")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("FOLLOW ME", url=f"https://t.me/vj_bots")
+                                InlineKeyboardButton("FOLLOW ME", url=f"https://t.me/SUDIPSAHA06")
                             ]
                         ]
                     ),
